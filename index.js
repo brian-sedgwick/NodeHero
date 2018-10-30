317119452f8275a868d0c18f092e327ae6028251
Brian Sedgwick <bsedgwick@directscale.com>
2018-10-30 16:36:11 -0600

    External API requests to AccuWeather

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-//require('./app/index')
-const path = require('path')
 const express = require('express')
-const app = express()
+const rp = require('request-promise')
 const exphbs = require('express-handlebars')
+
+const app = express()
+
 const port = 3000
 
 app.engine('.hbs', exphbs({
-defaultLayout: 'main',
-extname: '.hbs',
-layoutsDir: path.join(__dirname, 'views/layouts')
+	defaultLayout: 'main',
+	extname: '.hbs',
+	layoutsDir: path.join(__dirname, 'views/layouts')
 }))
+
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (request, response) => {
-	response.render('home', {
-		name: 'Brian'
+app.get('/:city', (req, res) => {
+	rp({
+		uri: 'http://apidev.accumweather.com/locations/v1/search',
+		qs: {
+			q: req.params.city,
+			apiKey: '6s4vzIDOsLTgmpnD7qieU02B33sFhTfU'
+		},
+		json: true
+	})
+	.then((data) => {
+		res.render('index', data)
+	})
+	.catch((err) => {
+		console.log(err)
+		res.render('error')
 	})
 })
 
