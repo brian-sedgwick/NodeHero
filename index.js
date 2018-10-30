b690ad7ed3ee3e64b9789447336dffa89bf74a97
Brian Sedgwick <bsedgwick@directscale.com>
2018-10-30 13:49:29 -0600

    Express handlebars view rendering.

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 //require('./app/index')
+const path = require('path')
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 const port = 3000
 
-app.use((request, response, next) => {
-	console.log(request.headers)
-	next()
-})
-
-app.use((request, response, next) => {
-	request.chance = Math.random()
-	next()
-})
+app.engine('.hbs', exphbs({
+defaultLayout: 'main',
+extname: '.hbs',
+layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (request, response) => {
-	throw new Error('oops')
-})
-
-app.use((err, request, response, next) => {
-	console.log(err)
-	response.status(500).send('something broke!')
+	response.render('home', {
+		name: 'Brian'
+	})
 })
 
 app.listen(port)
